448bc604ad0ea97008c44cad2968fdc4d6b714d0
James <james@jamesssystems.info>
2025-05-21 17:24:35 -0700

    base

---
+++ server.py
@@ -0,0 +1,46 @@
+from fastmcp import FastMCP
+import requests
+
+mcp = FastMCP("Library of Aletheia")
+
+@mcp.tool()
+def search_books(search_term: str):
+    """Search for books with the given search_term. Returns a list of books and their data."""
+    url = "https://searchv2.aletheia.foundation/collections/books/documents/search"
+    headers = {
+        "X-TYPESENSE-API-KEY": "***REMOVED_SEARCH_KEY***"
+    }
+    params = {
+        "q": search_term,
+        "query_by": "contents,title,author,tags",
+        "page": 1,
+        "page_size": 10
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
+
+@mcp.tool()
+def get_book(book_id: str):
+    """Get a book with the given book_id (in uuidv4 format)"""
+    url = f"https://api.aletheia.foundation/books/{book_id}"
+    headers = {
+        "Authorization": "Bearer ***REMOVED_ALETHEIA_KEY*** "
+    }
+
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+@mcp.tool()
+def get_user(user_name: str):
+    """Search users by their Roblox username."""
+    url = f"https://api.aletheia.foundation/users/search/{user_name}"
+    headers = {
+        "Authorization": "Bearer ***REMOVED_ALETHEIA_KEY*** "
+    }
+
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+if __name__ == "__main__":
+    mcp.run()
